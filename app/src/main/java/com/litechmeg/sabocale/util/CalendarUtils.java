9647fd3630b996e583981737912a38b3645a32bd
Fukuo Kadota <app.fk91@gmail.com>
2015-10-06 00:36:40 +0900

    Create some utils

---
+++ app/src/main/java/com/litechmeg/sabocale/util/CalendarUtils.java
@@ -0,0 +1,7 @@
+package com.litechmeg.sabocale.util;
+
+/**
+ * Created by fukuo on 2015/10/05.
+ */
+public class CalendarUtils {
+}
---
+++ app/src/main/java/com/litechmeg/sabocale/util/IntentUtils.java
@@ -0,0 +1,7 @@
+package com.litechmeg.sabocale.util;
+
+/**
+ * Created by fukuo on 2015/10/06.
+ */
+public class IntentUtils {
+}
